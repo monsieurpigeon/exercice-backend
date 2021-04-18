7209e4ca8e6c618afc83d66100134c783265de97
Maxime PIGEON <maxpige@gmail.com>
2021-04-18 19:44:04 +0200

    update server to read filesystem

--- src/FileService/resolver.ts
+++ src/FileService/resolver.ts
@@ -5,17 +5,25 @@ import {
 } from "type-graphql";
 
 import { File } from "./type";
+import { readdir } from 'fs';
+
+
 
 @Resolver(_of => File)
 export class FileResolver {
-  private readonly items: File[] = [
-    {"path": "aa", "attributes":"aa", "size": 5},
-    {"path": "aa", "attributes":"bb", "size": 5},
-    {"path": "aa", "attributes":"cc", "size": 5}
-  ];
 
   @Query(_returns => [File], { nullable: true, description: "Get all the files from a folder's path" })
-  async file(@Arg("path") path: string): Promise<File[] | undefined> {
-    return await this.items.filter(file => file.path === path);
+  async files(@Arg("path") path: string): Promise<File[] | undefined> {
+    const files: string[] = await new Promise((resolve, reject) => {
+      readdir(path, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      })
+    })
+    return files.map((file) => {
+      return {
+        path, size: 5, attributes: file
+      }
+    })
   }
 }
